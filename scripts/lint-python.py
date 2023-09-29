6e98410f893ca33110dbc1350e1d91f088ba17ae
Michael Graeb <graebm@amazon.com>
2023-09-29 11:37:14 -0700

    Rudimentary CI (just formatting checks for now)

---
+++ scripts/lint-python.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+import argparse
+from pathlib import Path
+import subprocess
+import sys
+
+PARSER = argparse.ArgumentParser(
+    description="Check formatting and type hints in python scripts")
+
+
+def run(cmd_args: list[str]):
+    print(f'> {subprocess.list2cmdline(cmd_args)}')
+    result = subprocess.run(cmd_args)
+    if result.returncode != 0:
+        exit('FAILED')
+
+
+def get_script_dirs() -> list[str]:
+    dirs = []
+    root = Path(__file__).parent.parent
+
+    # add this scripts/ dir
+    dirs.append(root.joinpath('scripts'))
+
+    # add each runner's scripts/ dir
+    for runner_child in root.joinpath('runners').iterdir():
+        if runner_child.is_dir():
+            runner_scripts = runner_child.joinpath('scripts')
+            if runner_scripts.is_dir():
+                dirs.append(runner_scripts)
+
+    return [str(i) for i in dirs]
+
+
+def check_formatting(dirs: list[str]):
+    cmd_args = [sys.executable, '-m', 'autopep8',
+                '--recursive', '--diff', '--exit-code']
+    cmd_args.extend(dirs)
+    run(cmd_args)
+
+
+def check_typing(dirs: list[str]):
+    # run mypy on each script dir separately,
+    # so it doesn't complain about there being multiple build.py files
+    for dir in dirs:
+        run([sys.executable, '-m', 'mypy', dir])
+
+
+if __name__ == '__main__':
+    args = PARSER.parse_args()
+    script_dirs = get_script_dirs()
+    check_formatting(script_dirs)
+    check_typing(script_dirs)

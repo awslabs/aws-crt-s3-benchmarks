94bfd985dd3d7e136a6d40368b63ff68fc366db1
Michael Graeb <graebm@amazon.com>
2023-11-13 22:33:02 +0000

    download is faster when it's not overwriting pre-existing files

--- runners/s3-benchrunner-python/runner/__init__.py
+++ runners/s3-benchrunner-python/runner/__init__.py
@@ -3,6 +3,7 @@ import io
 import json
 import math
 import os
+from pathlib import Path
 import sys
 
 
@@ -102,6 +103,19 @@ class BenchmarkRunner:
                 if task.action == 'upload' and task.size > largest_upload:
                     largest_upload = task.size
             self._random_data_for_upload = os.urandom(largest_upload)
+
+    def prepare_run(self):
+        """Do work between runs, before the timer starts."""
+        for task in self.config.tasks:
+            if task.action == 'download':
+                task_path = Path(task.key)
+                if task_path.exists():
+                    # CLI and boto3 download to a tmp filename, then rename to the final filename.
+                    # The rename is way slower if that rename overwrites an existing file.
+                    # We want fast in the benchmarks, so ensure there's no pre-existing file.
+                    task_path.unlink()
+                elif not task_path.parent.exists:
+                    task_path.parent.mkdir(parents=True)
 
     def run(self):
         raise NotImplementedError()

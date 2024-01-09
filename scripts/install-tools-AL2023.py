a79e1995ead4687879d56139beeab04edaa1907a
Michael Graeb <graebm@amazon.com>
2024-01-09 10:19:13 -0800

    consolidate python scripts (#37)
    **Issue:**
    It was hard to share anything between the python scripts, because they were spread around the codebase.
    
    **Description of changes:**
    * Create `scripts/utils/` folder for shared bits of python scripting.
    * Move N different ~runners/s3-benchrunner-X/scripts/build.py~  to a single `scripts/build-runner.py --lang X`
    * Move N different ~runners/s3-benchrunner-X/scripts/lint.py~ to a single `scripts/lint.py --lang X`
    * Rename ~s3-benchrunner-crt-java~ to `s3-benchrunner-java` for consistency with other runners. When we add more java libraries (e.g. aws-sdk-java-v2 transfer manager) to the benchmarks, they'll live within this same runner

--- scripts/install-tools-AL2023.py
+++ scripts/install-tools-AL2023.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
 import argparse
-import subprocess
+
+from utils import run
 
 parser = argparse.ArgumentParser(
     description="Install tools needed to build and run. Assumes we're on Amazon Linux 2023")
 args = parser.parse_args()
-
-
-def run(cmd_args: list[str]):
-    print(f'> {subprocess.list2cmdline(cmd_args)}', flush=True)
-    subprocess.run(cmd_args, check=True)
 
 
 run(['dnf', 'install', '-y',

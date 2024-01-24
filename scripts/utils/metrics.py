58259a4bd1ff884db320a067c2e1b24eb70c24e6
Michael Graeb <graebm@amazon.com>
2024-01-24 15:06:37 -0800

    remove unnecessary dimensions from metrics

--- scripts/utils/metrics.py
+++ scripts/utils/metrics.py
@@ -27,38 +27,12 @@ def report_metrics(*,
     if run_count == 0:
         return
 
-    # get data from workload
-    with open(workload_path) as f:
-        workload = json.load(f)
-
-    num_files = 0
-    total_file_size = 0
-    action = None  # will be "upload", "download", or "mixed"
-    for task in workload['tasks']:
-        num_files += 1
-        total_file_size += task['size']
-
-        if action is None:
-            action = task['action']
-        elif action != task['action']:
-            action = 'mixed'
-
-    avg_file_size = int(total_file_size / num_files)
-
     # prepare metrics data
     dimensions = [
         {'Name': 'S3Client', 'Value': s3_client_id},
         {'Name': 'InstanceType', 'Value': instance_type or 'Unknown'},
-        {'Name': 'TargetThroughput', 'Value': str(target_throughput_Gbps)},
         {'Name': 'Branch', 'Value': branch or 'Unknown'},
-        {'Name': 'Workload', 'Value': workload_path.name.split('.')[0]},
-        {'Name': 'Bucket', 'Value': bucket},
-        {'Name': 'Action', 'Value': action},
-        {'Name': 'FileLocation',
-            'Value': 'Disk' if workload['filesOnDisk'] else 'RAM'},
-        {'Name': 'NumFiles', 'Value': str(num_files)},
-        {'Name': 'TotalSize', 'Value': _pretty_file_size(total_file_size)},
-        {'Name': 'AvgFileSize', 'Value': _pretty_file_size(avg_file_size)},
+        {'Name': 'Workload', 'Value': 'main'}, # DO NOT COMMIT DO NOT COMMIT workload_path.name.split('.')[0]},
     ]
 
     metric_data = []

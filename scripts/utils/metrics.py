2ed83894d347de7db501c3a761e27fe1fe95b4a5
Waqar Ahmed Khan <waahm7@gmail.com>
2024-04-08 11:47:10 -0700

    Metric for seconds as well

--- scripts/utils/metrics.py
+++ scripts/utils/metrics.py
@@ -20,6 +20,8 @@ def report_metrics(*,
     # parse stdout
     throughput_per_run_Gbps = _given_stdout_get_list_throughput_per_run_in_gigabits(
         run_stdout)
+    seconds_per_run = _given_stdout_get_list_seconds_per_run(run_stdout)
+
     run_count = len(throughput_per_run_Gbps)
 
     # bail out if no successful runs
@@ -58,6 +60,24 @@ def report_metrics(*,
             'Dimensions': dimensions,
         })
 
+    for run_idx, seconds in enumerate(seconds_per_run):
+
+        # if we had multiple runs, don't report the first run,
+        # since things are warming up (connection pools, file caching, etc)
+        if run_idx == 0 and run_count > 1:
+            continue
+
+        approx_timestamp = run_start_time + \
+            approx_duration_per_run * (run_idx + 1)
+
+        metric_data.append({
+            'MetricName': 'Duration',
+            'Value': seconds,
+            'Unit': 'Second',
+            'Timestamp': approx_timestamp,
+            'Dimensions': dimensions,
+        })
+
     print('Reporting metrics...')
     cloudwatch_client = boto3.client('cloudwatch', region_name=region)
     cloudwatch_client.put_metric_data(
@@ -90,3 +110,29 @@ def _given_stdout_get_list_throughput_per_run_in_gigabits(stdout: str) -> list[f
         gigabits_per_sec = float(m.group(1))
         throughput_per_run.append(gigabits_per_sec)
     return throughput_per_run
+
+
+def _given_stdout_get_list_seconds_per_run(stdout: str) -> list[float]:
+    """
+    Examine stdout from runner, and return the seconds for each run.
+
+    For example, given:
+    '''
+    [ERROR] [2024-01-10T22:46:03Z] [00007f4124174440] [AuthCredentialsProvider] - ...
+    Run:1 Secs:8.954437 Gb/s:28.847134
+    Run:2 Secs:9.180856 Gb/s:28.116831
+    Run:3 Secs:9.321967 Gb/s:27.612145
+    Done!
+    '''
+
+    Returns [28.847134, 28.116831, 27.612145]
+    """
+    pattern = re.compile(r'^Run:\d+ .* Secs:(\d+\.\d+) .*')
+    seconds_per_run = []
+    for line in stdout.splitlines():
+        m = pattern.match(line)
+        if not m:
+            continue
+        seconds = float(m.group(1))
+        seconds_per_run.append(seconds)
+    return seconds_per_run

a6272140ee45cb51247a499708ea4e73e290db8d
Waqar Ahmed Khan <waahm7@gmail.com>
2024-04-09 10:47:37 -0700

    Sleep for 3 seconds

--- runners/s3-benchrunner-c/benchrunner.cpp
+++ runners/s3-benchrunner-c/benchrunner.cpp
@@ -647,7 +647,7 @@ int main(int argc, char *argv[])
         duration<double> appDurationSecs = high_resolution_clock::now() - appStart;
         if (appDurationSecs >= 1s * config.maxRepeatSecs)
             break;
-        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::this_thread::sleep_for(std::chrono::seconds(3));
     }
 
     printStats(bytesPerRun, durations);

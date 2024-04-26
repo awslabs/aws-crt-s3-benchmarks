c84845b7769cb98435af76b2c83bd72f8f666fec
Waqar Ahmed Khan <waahm7@gmail.com>
2024-04-26 11:12:46 -0700

    all lowercase?

--- cdk/s3_benchmarks/s3_benchmarks_stack.py
+++ cdk/s3_benchmarks/s3_benchmarks_stack.py
@@ -189,8 +189,8 @@ class S3BenchmarksStack(Stack):
         # The device path format is /dev/nvme[0-26]n1. /dev/nvme0n1 will be the EBS volume and the first instance storage device path will be /dev/nvme1n1
         # See https://docs.aws.amazon.com/ebs/latest/userguide/nvme-ebs-volumes.html
         commands_user_data.add_commands('mkfs -t xfs /dev/nvme1n1')
-        commands_user_data.add_commands('mkdir /s3work')
-        commands_user_data.add_commands('mount /dev/nvme1n1 /s3work')
+        commands_user_data.add_commands('mkdir /uniquework')
+        commands_user_data.add_commands('mount /dev/nvme1n1 /uniquework')
 
         self.per_instance_launch_templates[s3_benchmarks.StorageConfiguration.INSTANCE_STORAGE] = ec2.LaunchTemplate(
             self, f"PerInstanceLaunchTemplateWithNVMeStorage",
@@ -250,7 +250,7 @@ class S3BenchmarksStack(Stack):
             ],
             job_role=self.per_instance_job_role,
             volumes=[batch.EcsVolume.host(container_path=s3_benchmarks.S3_BENCHMARKS_WORK_BASE_DIR, host_path=s3_benchmarks.S3_BENCHMARKS_WORK_BASE_DIR,
-                                          name="s3work")] if instance_type.storage_configuration == s3_benchmarks.StorageConfiguration.INSTANCE_STORAGE else None,
+                                          name="uniquework")] if instance_type.storage_configuration == s3_benchmarks.StorageConfiguration.INSTANCE_STORAGE else None,
         )
 
         job_defn = batch.EcsJobDefinition(

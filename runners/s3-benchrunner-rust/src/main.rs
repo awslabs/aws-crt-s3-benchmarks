b20dfaad875ec626f7a394db1aeb4b5c9a205bf5
Michael Graeb <graebm@amazon.com>
2024-07-24 13:45:51 -0700

    s3-benchrunner-rust can download (#66)
    * Add tokio runtime
    * Actually use the Downloader from aws-s3-transfer-manager-rs
    * Run all downloads in a workload concurrently
      * This requires manually divvying up concurrency between downloads, since the Transfer Manager doesn't have a global scheduler yet.

--- runners/s3-benchrunner-rust/src/main.rs
+++ runners/s3-benchrunner-rust/src/main.rs
@@ -3,7 +3,8 @@ use std::process::exit;
 use std::time::Instant;
 
 use s3_benchrunner_rust::{
-    bytes_to_gigabits, BenchmarkConfig, BenchmarkRunner, Result, RunnerError, TransferManagerRunner,
+    bytes_to_gigabits, prepare_run, BenchmarkConfig, Result, RunBenchmark, RunnerError,
+    TransferManagerRunner,
 };
 
 #[derive(Parser)]
@@ -33,7 +34,12 @@ enum S3ClientId {
 fn main() {
     let args = Args::parse();
 
-    match run_benchmark(&args) {
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    match runtime.block_on(async_main(&args)) {
         Err(RunnerError::Fail(e)) => {
             panic!("{e:?}");
         }
@@ -45,7 +51,7 @@ fn main() {
     }
 }
 
-fn run_benchmark(args: &Args) -> Result<()> {
+async fn async_main(args: &Args) -> Result<()> {
     let config = BenchmarkConfig::new(
         &args.workload,
         &args.bucket,
@@ -54,8 +60,11 @@ fn run_benchmark(args: &Args) -> Result<()> {
     )?;
 
     // create appropriate benchmark runner
-    let runner: Box<dyn BenchmarkRunner> = match args.s3_client {
-        S3ClientId::TransferManager => Box::new(TransferManagerRunner::new(config)),
+    let runner: Box<dyn RunBenchmark> = match args.s3_client {
+        S3ClientId::TransferManager => {
+            let transfer_manager = TransferManagerRunner::new(config).await;
+            Box::new(transfer_manager)
+        }
     };
     let workload = &runner.config().workload;
     let bytes_per_run: u64 = workload.tasks.iter().map(|x| x.size).sum();
@@ -64,9 +73,11 @@ fn run_benchmark(args: &Args) -> Result<()> {
     // repeat benchmark until we exceed max_repeat_count or max_repeat_secs
     let app_start = Instant::now();
     for run_i in 0..workload.max_repeat_count {
+        prepare_run(workload)?;
+
         let run_start = Instant::now();
 
-        runner.run();
+        runner.run().await?;
 
         let run_secs = run_start.elapsed().as_secs_f64();
         println!(

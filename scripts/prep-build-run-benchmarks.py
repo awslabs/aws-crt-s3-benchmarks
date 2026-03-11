0477bfddf93403419413fcf8ea1cd7a62126125a
Dengke Tang <dengket@amazon.com>
2026-03-11 12:38:43 -0700

    add verbose to check what's the error?

--- scripts/prep-build-run-benchmarks.py
+++ scripts/prep-build-run-benchmarks.py
@@ -90,6 +90,7 @@ if __name__ == '__main__':
                 '--throughput', str(args.throughput),
                 '--files-dir', str(files_dir),
                 '--workloads', *[str(x) for x in workloads],
+                '--verbose',
             ]
             if args.report_metrics:
                 run_cmd += ['--report-metrics']
